5d254c514e86e0f2535683783d543d4f6ffb5f94
EduardoPaixao <eduardo.mmpj@gmail.com>
2019-04-18 23:09:20 -0300

    edu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdocaoComponent } from './adocao/adocao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdocaoComponent
   ],
   imports: [
     BrowserModule,
