26d9dc7f5577758250acfbafddc0763a10539bda
EduardoPaixao <eduardo.mmpj@gmail.com>
2019-04-19 00:32:41 -0300

    edu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './componente/login/login.component';
+import { AdocaoComponent } from './componente/adocao/adocao.component';
 
 const routes: Routes = [
   {path : '', component : LoginComponent},
+  {path : 'Adocao', component: AdocaoComponent}
 ];
 
 @NgModule({
