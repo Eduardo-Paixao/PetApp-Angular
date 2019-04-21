99ba04cf1f3d6aee371a1545ff7ff9229cb23325
RamonSales62 <48219259+RamonSales62@users.noreply.github.com>
2019-04-21 20:12:03 -0300

    ramon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdocaoComponent } from './componente/adocao/adocao.component';
 import { LoginComponent } from './componente/login/login.component';
-
 import { CadastroPetComponent } from './componente/cadastro-pet/cadastro-pet.component';
 import { CadastroComponent } from './componente/cadastro/cadastro.component';
 import { AnimaisComponent } from './componente/animais/animais.component';
-<<<<<<< HEAD
 import { Animal1Component } from './componente/animal1/animal1.component';
 import { Animal2Component } from './componente/animal2/animal2.component';
 import { Animal3Component } from './componente/animal3/animal3.component';
@@ -17,17 +15,12 @@ import { Animal4Component } from './componente/animal4/animal4.component';
 import { Animal5Component } from './componente/animal5/animal5.component';
 import { Animal6Component } from './componente/animal6/animal6.component';
 
-=======
-import { CadastroPetComponent } from './componente/cadastro-pet/cadastro-pet.component';
-import { CadastroComponent } from './componente/cadastro/cadastro.component';
->>>>>>> d1b01a615614bd72a72220ee4acc22cdc8f7164e
 
 @NgModule({
   declarations: [
     AppComponent,
     AdocaoComponent,
     LoginComponent,
-<<<<<<< HEAD
     CadastroPetComponent,
     CadastroComponent,
     AnimaisComponent,
@@ -36,14 +29,8 @@ import { CadastroComponent } from './componente/cadastro/cadastro.component';
     Animal3Component,
     Animal4Component,
     Animal5Component,
-    Animal6Component,
+    Animal6Component
 
-
-=======
-    AnimaisComponent,
-    CadastroPetComponent,
-    CadastroComponent
->>>>>>> d1b01a615614bd72a72220ee4acc22cdc8f7164e
   ],
   imports: [
     BrowserModule,
